d16f7b7b2a99dbb52f41bc3437c449438583bd86
Daniel Trakowsky <daniel070290@gmail.com>
2020-12-06 15:11:29 +0100

    V1

---
+++ Auto Fill.ino
@@ -0,0 +1,39 @@
+/*
+ Name:		Auto_Fill.ino
+ Author:	Daniel Trakowsky aka JustTheD
+*/
+
+#define Pump 10
+#define SensH 12
+#define SensL 11
+
+
+
+
+void setup() {
+	pinMode(SensH, INPUT_PULLUP); pinMode(SensL, INPUT_PULLUP);
+	pinMode(Pump, OUTPUT);
+	digitalWrite(SensH, HIGH); digitalWrite(SensL, HIGH);
+	digitalWrite(Pump, LOW);
+
+
+}
+
+void loop() {
+  
+
+
+	if(digitalRead(SensH) == HIGH) 
+	{
+		if (digitalRead(SensL) == HIGH) 
+		{
+			while (digitalRead (SensH)== HIGH)
+			{
+				digitalWrite(Pump, HIGH);
+			}
+			digitalWrite(Pump, LOW);
+		}
+	}
+	
+
+}
